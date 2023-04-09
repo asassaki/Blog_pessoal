0e8606a30320b0342490affd80c7e043175809f9
alinesassaki@hotmail.com <alinesassaki@hotmail.com>
2023-04-09 16:18:33 -0300

    Added 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjE5NDY3MA/spectral.js?branch_slug=main&token=8e9e00e3-1d05-40b9-ba77-a1082c645ed1";
+export default { extends: ruleset };
